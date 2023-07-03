7ea2ce452f2c22d6b9be4731087a879fb89a561c
spatronn <safaphlvnlr@gmail.com>
2023-07-04 00:15:18 +0300

    Update malicious_saves.py

--- malicious_saves.py
+++ malicious_saves.py
@@ -20,12 +20,11 @@ def get_req(x,y):
     file_type = magic.from_file(saved_file)
 
     if "ELF 32-bit MSB executable" in file_type:
-        return magic.from_file(saved_file)
+        print(magic.from_file(saved_file))
         with open(saved_file, 'rb') as f:
             data = f.read()
             sha256hash = hashlib.sha256(data).hexdigest().encode('utf-8')
-            return "SHA256:", sha256hash
-
+            print("SHA256:", sha256hash.decode('ASCII'))
     else:
         pass
 
