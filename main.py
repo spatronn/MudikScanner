eb4e2a513bb845de8d4c3fb47c3ffdffc8a9acae
spatronn <safaphlvnlr@gmail.com>
2023-07-04 00:12:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ from scapy.all import *
 from datetime import datetime
 from multiprocessing import *
 import functools
+import sqlite3
+
 
 startTime = datetime.now()
 
-dst_ip = "92.205.182.247"
+dst_ip = "123.14.32.31"
 dst_port = 1024
-range01 = range(1024,16127)
-range02 = range(16127,32254)
-range03 = range(32254,48381)
-range04 = range(48381,65536)
+range01 = range(34690,34700)
+range02 = range(34700,34710)
+range03 = range(34710,34720)
+range04 = range(34720,34730)
+
+
 
 def syn_send(param_dst_ip,param_dst_port):
     syn_package = IP(dst=param_dst_ip, id=RandShort(), ttl=99) / TCP(sport=RandShort(), dport=[param_dst_port],seq=RandShort(), ack=RandShort(),window=RandShort(), flags="S")
-    ans = sr1(syn_package, verbose=0, timeout=0.3) ## Timeout depends by many factors (-_-)
+    ans = sr1(syn_package, verbose=0, timeout=2)
     try:
         pktflags = ans.getlayer(TCP).flags
         if pktflags == None:
             pass
         else:
             if pktflags == "SA":
-                print("Port Open:", param_dst_port)
+                print("Port Open:", param_dst_port,param_dst_ip)
+                conn = sqlite3.connect('maldb.db', timeout=15)
+                now = datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M")
+                conn.execute("INSERT INTO open_port (time,dst_ip_addr,dst_port) VALUES (?, ?, ?)", (dt_string, param_dst_ip, param_dst_port))
+                conn.commit()
+                conn.close()
+
             else:
                 pass
     except:
